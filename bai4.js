77876c4899a0b7bc19cd1fe4238f8d42faa16b7c
namtiennguyen <toilanam97@mail.com>
2019-08-01 09:44:40 +0700

    bai 4

---
+++ bai4.js
@@ -0,0 +1,5 @@
+let d = Number(prompt('Nhập đường kính: '));
+let v = d * 3.14;
+document.write("Đường kính hình tròn là: " + d);
+document.write("</br>");
+document.write("Chu vi hình tròn là: "+ v);
